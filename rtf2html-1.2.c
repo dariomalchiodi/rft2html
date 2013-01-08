a5b1aa434ca9a3fb5d651ed12fc4d3eebeff709f
Dario Malchiodi <malchiodi@di.unimi.it>
2013-01-08 20:59:51 +0100

    Added license and copyright statements

--- rtf2html-1.2.c
+++ rtf2html-1.2.c
@@ -1,3 +1,21 @@
+
+/******************************************************************************
+       Copyright (C) 2010 Dario Malchiodi <malchiodi@di.unimi.it>
+
+This file is part of rtf2html.
+rtf2html is free software; you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2.1 of the License, or (at your option)
+any later version.
+rtf2html is distributed in the hope that it will be useful, but without any
+warranty; without even the implied warranty of merchantability or fitness
+for a particular purpose. See the GNU Lesser General Public License for
+more details.
+You should have received a copy of the GNU Lesser General Public License
+along with rtf2html; if not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************/
+
 /*RTF2HTML.c, Chuck Shotton - 6/21/93 */
 /************************************************************************
  * This program takes a stab at converting RTF (Rich Text Format) files
